13f9780c4911c8327a85f0ab6c354e06225785e0
Scottie <smb510@gmail.com>
2012-02-02 23:31:19 -0500

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NIRCA::Application.config.secret_token = 'd1af492e650cc537110e7b1ac9979a8096600b192fb977655e4559e63839c0b00edba9e2e1eda321cd6dcc7d61d3b5f9aae492f0686ec395c9c0241ca327ceca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+NIRCA::Application.config.session_store :cookie_store, :key => '_NIRCA_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# NIRCA::Application.config.session_store :active_record_store
