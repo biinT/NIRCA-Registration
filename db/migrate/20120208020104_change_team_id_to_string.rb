046e929f5a03101516205aa4693a9bc3cdd9d4b6
Scottie <smb510@gmail.com>
2012-02-08 22:02:01 -0500

    fixed annoying postgresql error

--- db/migrate/20120208020104_change_team_id_to_string.rb
+++
@@ -1,9 +0,0 @@
-class ChangeTeamIdToString < ActiveRecord::Migration
-  def up
-    change_column :runners, :team_id, :integer
-  end
-
-  def down
-    change_column :runners, :team_id, :string
-  end
-end
